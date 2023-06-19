0815e577922222214922434e089d50b9b62d52da
dan <danny.453@hotmail.com>
2023-06-19 19:43:09 -0400

    finish

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import "./app.scss";
+
+function App() {
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo-items')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('todo-items', JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <div id="app">
+      <h1>My Todo 🗒️</h1>
+
+      <div className="container">
+        <form>
+          <h2>Add Item</h2>
+          <input
+            type="text"
+            id="title"
+            placeholder="Title"
+            className="form-control"
+            autocomplete="off"
+            value={input}
+            onChange={(event) => {
+              setInput(event.target.value)
+
+            }}
+            
+          />
+          <button
+          //type="button"
+          onClick={(event)=> {
+            event.preventDefault()
+            if(!input){
+            alert('Value cannot be empty!')
+            return;
+            }
+            const newItems = [...items];
+            newItems.push(input);
+            setItems(newItems);
+            setInput('');
+          }}
+          >
+            Add
+          </button>
+          <button
+          className="orange"
+          style={{
+            backgroundColor: `orange !important` 
+          }}
+
+          onClick={() => {
+            setItems([]);
+          }}
+          >
+            Reset
+          </button>
+        </form>
+
+        <div className="items">
+          <h2>My Items</h2>
+          <ol>
+            {
+            items.map((item, index) => {
+              return (
+                <li key={`item-${index}`}>
+                  <h3>
+                    {item}
+                    </h3>
+                    <button
+                      className="delete"
+                      onClick={()=> {
+                        const newItems = items.filter((item, indexItem) => {
+                          return (index !== indexItem);
+                        })
+                        /* const newItems = 
+                        [...item.slice(0, index), 
+                          ...item(index+ 1, item.length -1)]
+ */
+                        setItems(newItems);
+                      }}
+                    ></button>
+                </li>
+              
+              );
+            })
+            }
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
